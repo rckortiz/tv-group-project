dccc61a2c34a3b0b4af961c0f3c5f0c8d2533476
Corey Grandmaison <cgsustainability@gmail.com>
2019-09-01 15:50:57 -0400

    working to push

--- src/components/ShowDetails.js
+++ src/components/ShowDetails.js
@@ -2,6 +2,22 @@ import React, { useState, useEffect } from "react"
 import axios from "axios"
 
 const ShowDetails = props => {
+  const [show, setShow] = useState([])
+  const [tvId, setTvId] = useState([])
+
+  const getSomeData = async () => {
+    const resp = await axios.get(
+      `https://api.themoviedb.org/3/tv/${tvId}/credits?api_key=60f58f18f96b65a50bd5cc31bafb3aac&language=en-US`
+    )
+    // setTvId(resp) might need to set this to the other api call
+    setShow(resp.data.results)
+    console.log(show)
+  }
+
+  useEffect(() => {
+    getSomeData()
+  })
+
   return (
     <div>
       <h1>{props.name}</h1>
