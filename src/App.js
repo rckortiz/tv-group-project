8514d73f46d5df943936afe4499f6783c8344bb2
Rick Ortiz <rick@Ricks-MBP.localdomain>
2019-09-01 15:31:52 -0400

    updated posters

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
-import LandingPage from './components/landingPage'
+import LandingPage from './components/LandingPage'
 
 class App extends Component {
   render() {
     return <LandingPage />
-=======
-import React, { Component } from "react"
-import ShowDetails from "./components/ShowDetails.js"
-
-class App extends Component {
-  render() {
-    return <ShowDetails />
->>>>>>> d76f9199f5f61c1d90f635ad66b98ac00caf9524
   }
 }
 
